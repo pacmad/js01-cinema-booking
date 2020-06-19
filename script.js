ee7673c12b0d5aa7a9d27109ba3e7a79ee318431
cheangfousuy <thecounterfeitcat@gmail.com>
2020-06-19 20:27:51 +0700

    first commit

--- script.js
+++ script.js
@@ -6,8 +6,25 @@ const movieSelect = document.getElementById('movie');
 
 let price = +movieSelect.value;
 
+// Save selected movie index and price
+function setMovieData(index, price){
+    //no need JSON.stringify, because it's not an array
+    localStorage.setItem('selectedMovieIndex', index);
+    localStorage.setItem('selextedMoviePrice', price);
+}
+
+// Update count and amount
 function updateSelectedCount(){
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    //get the index and save it as an array
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+    //because it's an array, so need to use JSON.stringify
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+
+
     const selectedSeatsCount = selectedSeats.length;
     
     count.innerText = selectedSeatsCount;
@@ -17,6 +34,7 @@ function updateSelectedCount(){
 // Movie select event
 movieSelect.addEventListener('click', (e) => {
     price = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 })
 
