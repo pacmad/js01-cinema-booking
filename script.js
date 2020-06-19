a1c7a5b2debfe61b8f1d6d0009166dc27857a14c
cheangfousuy <thecounterfeitcat@gmail.com>
2020-06-19 20:02:36 +0700

    first commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const count = document.getElementById('count');
 const amount = document.getElementById('amount');
 const movieSelect = document.getElementById('movie');
 
-const price = +movieSelect.value;
+let price = +movieSelect.value;
 
 function updateSelectedCount(){
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
@@ -14,6 +14,13 @@ function updateSelectedCount(){
     amount.innerText = selectedSeatsCount * price;
 }
 
+// Movie select event
+movieSelect.addEventListener('click', (e) => {
+    price = +e.target.value;
+    updateSelectedCount();
+})
+
+// Seats select event
 container.addEventListener('click', (e) => {
     if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
         e.target.classList.toggle('selected');
